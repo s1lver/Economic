bc2df405098aef51f85c330001cb3567f0a02827
s1lver <s1lverhron@gmail.com>
2021-10-26 20:48:31 +0300

    Refactor and clear code

--- src/SubModule.cs
+++ src/SubModule.cs
@@ -4,25 +4,13 @@ using System.Linq;
 using System.Xml.Serialization;
 using System.Runtime;
 using Helpers;
-using TaleWorlds.CampaignSystem;
 using TaleWorlds.Core;
-using TaleWorlds.CampaignSystem.Actions;
-using TaleWorlds.CampaignSystem.CharacterDevelopment.Managers;
-using TaleWorlds.Localization;
 using TaleWorlds.MountAndBlade;
-using TaleWorlds.Library;
-using TaleWorlds.Library.NewsManager;
-using TaleWorlds.SaveSystem;
 
 namespace Economic
 {
     public class SubModule : MBSubModuleBase
     {
-        protected override void OnSubModuleLoad()
-        {
-            base.OnSubModuleLoad();
-        }
-
         public override void OnGameLoaded(Game game, object initializerObject)
         {
             base.OnGameLoaded(game, initializerObject);
